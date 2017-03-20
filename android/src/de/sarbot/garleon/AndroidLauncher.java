d408676c672290a66b1b9cb99f1228c1fd01e093
sarbot <mail@sarbot.de>
2017-03-20 15:27:14 +0100

    libgdx projekt erstellt

---
+++ android/src/de/sarbot/garleon/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package de.sarbot.garleon;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import de.sarbot.garleon.GarleonGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GarleonGame(), config);
+	}
+}
---
+++ desktop/src/de/sarbot/garleon/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.sarbot.garleon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.sarbot.garleon.GarleonGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GarleonGame(), config);
+	}
+}
