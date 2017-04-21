33fbd8aab5edfc3f74e9c010f10365a4cad00f37
sarbot <mail@sarbot.de>
2017-04-21 15:57:21 +0200

    character dialog layout concept

--- core/src/de/sarbot/garleon/Objects/Interface.java
+++ core/src/de/sarbot/garleon/Objects/Interface.java
@@ -1,10 +1,12 @@
-package de.sarbot.garleon.Objects.Ui;
+package de.sarbot.garleon.Objects;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
@@ -14,7 +16,6 @@ import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.viewport.FillViewport;
 import de.sarbot.garleon.GarleonGame;
-import de.sarbot.garleon.Objects.*;
 import de.sarbot.garleon.Tools;
 
 import java.util.ArrayList;
